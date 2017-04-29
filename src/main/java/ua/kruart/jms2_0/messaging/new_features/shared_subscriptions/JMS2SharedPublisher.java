071ef3c21060e7ca0768dfb3271f9fe0875395cc
weoz <weoz@ukr.net>
2017-04-29 23:17:43 +0300

    jms2.0v new features

---
+++ src/main/java/ua/kruart/jms2_0/messaging/new_features/shared_subscriptions/JMS2SharedPublisher.java
@@ -0,0 +1,26 @@
+package ua.kruart.jms2_0.messaging.new_features.shared_subscriptions;
+
+import com.sun.messaging.ConnectionFactory;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Topic;
+import java.text.DecimalFormat;
+
+/**
+ * Created by kruart on 29.04.2017.
+ */
+public class JMS2SharedPublisher {
+
+    public static void main(String[] args) throws JMSException {
+        ConnectionFactory cf = new ConnectionFactory();
+
+        try (JMSContext jmsContext = cf.createContext()) {
+            Topic topic = jmsContext.createTopic("EM_JMS2_TRADE.T");
+            DecimalFormat df = new DecimalFormat("##.00");
+            String price = df.format(98.0 + Math.random());
+            jmsContext.createProducer().send(topic, price);
+            System.out.println("Message published");
+        }
+    }
+}
