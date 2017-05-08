de1768683a6b93630eb8f40ab309c3c00d2193ab
weoz <weoz@ukr.net>
2017-05-08 16:59:01 +0300

    jms metadata

---
+++ src/main/java/ua/kruart/jms/connection_metadata/JMS2MetaData.java
@@ -0,0 +1,34 @@
+package ua.kruart.jms.connection_metadata;
+
+import com.sun.messaging.ConnectionFactory;
+
+import javax.jms.ConnectionMetaData;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import java.util.Enumeration;
+
+/**
+ * Created by kruart on 08.05.2017.
+ */
+public class JMS2MetaData {
+
+    public static void main(String[] args) {
+        ConnectionFactory cf = new ConnectionFactory();
+        try(JMSContext jmsContext = cf.createContext()) {
+            ConnectionMetaData metadata = jmsContext.getMetaData();
+            System.out.println(" ");
+            System.out.println("JMS Version: " + metadata.getJMSVersion());
+            System.out.println("JMS Provider: " + metadata.getJMSProviderName());
+            System.out.println("JMS Provider version: " + metadata.getProviderVersion());
+            System.out.println("JMSX Properties Supported: ");
+
+            Enumeration<?> jmsxPropertyNames = metadata.getJMSXPropertyNames();
+            while (jmsxPropertyNames.hasMoreElements()) {
+                System.out.println(" " + jmsxPropertyNames.nextElement());
+            }
+
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
