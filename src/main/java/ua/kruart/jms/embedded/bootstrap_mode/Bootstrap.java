19098d7cee2d9738199a9ad2bef26d6005e920e7
weoz <weoz@ukr.net>
2017-05-08 14:35:34 +0300

    embedded broker(Bootstrap and VM)

---
+++ src/main/java/ua/kruart/jms/embedded/bootstrap_mode/Bootstrap.java
@@ -0,0 +1,27 @@
+package ua.kruart.jms.embedded.bootstrap_mode;
+
+import org.apache.activemq.broker.BrokerService;
+
+/**
+ * Created by kruart on 08.05.2017.
+ */
+public class Bootstrap {
+
+    public void startBroker() {
+        try {
+            BrokerService broker = new BrokerService();
+            broker.addConnector("tcp://localhost:61888");
+            broker.setBrokerName("embedded1");
+            broker.setPersistent(false); //doesn't persist any message
+            broker.start();
+
+            System.out.println("embedded broker started");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(() -> new Bootstrap().startBroker()).start();
+    }
+}
