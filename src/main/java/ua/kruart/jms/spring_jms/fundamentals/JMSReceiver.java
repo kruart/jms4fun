1a6b060be148837e77720c19a0b2fa944b9faba8
weoz <weoz@ukr.net>
2017-05-09 21:17:28 +0300

    spring-jms JmsTemplate

--- src/main/java/ua/kruart/jms/spring_jms/fundamentals/JMSReceiver.java
+++ src/main/java/ua/kruart/jms/spring_jms/fundamentals/JMSReceiver.java
@@ -2,12 +2,27 @@ package ua.kruart.jms.spring_jms.fundamentals;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+
+import javax.jms.TextMessage;
 
 public class JMSReceiver {
 
 	public static void main(String[] args) throws Exception {
 		
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring_jms_fundamentals/app-context.xml");
+		JmsTemplate jmsTemplate = (JmsTemplate)ctx.getBean("jmsTemplate");
+		System.out.println("Waiting for messages...");
+
+		// first way to receive message
+//		String bodyMSG = (String) jmsTemplate.receiveAndConvert();	//just receive message
+//		System.out.println(bodyMSG);
+
+		// second way to receive message and message property
+//		TextMessage msg = (TextMessage)jmsTemplate.receive();
+		TextMessage msg = (TextMessage)jmsTemplate.receiveSelected("Trader = 'Arthur'"); //if I want to receive messages on from Arthur
+		System.out.println(msg.getStringProperty("Trader") + ", " + msg.getText());
+
 		System.exit(0);
 	}
 }
--- src/main/java/ua/kruart/jms/spring_jms/fundamentals/JMSSender.java
+++ src/main/java/ua/kruart/jms/spring_jms/fundamentals/JMSSender.java
@@ -2,12 +2,37 @@ package ua.kruart.jms.spring_jms.fundamentals;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessagePostProcessor;
 
 public class JMSSender {
 
 	public static void main(String[] args) throws Exception {
 		
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring_jms_fundamentals/app-context.xml");
+		JmsTemplate jmsTemplate = (JmsTemplate)ctx.getBean("jmsTemplate");
+
+		// first way to send message
+		//		jmsTemplate.convertAndSend("BUY AAPL 2500 SHARES");		//just send message
+
+		// second way to send message and message property
+//		MessageCreator mc = (session -> {	//send message with property
+//            TextMessage msg = session.createTextMessage("BUY AAPL 2500 SHARES");
+//            msg.setStringProperty("Trader", "Arthur");
+//            return msg;
+//        });
+//
+//		jmsTemplate.send(mc);
+
+		// third way to send message and message property
+		MessagePostProcessor mpp = msg -> {
+            msg.setStringProperty("Trader", "Arthur");
+            return msg;
+        };
+
+		jmsTemplate.convertAndSend((Object) "BUY AAPL 2500 SHARES", mpp);
+		System.out.println("Message sent...");
+
 		System.exit(0);
 	}
 }
